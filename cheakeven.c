1cdfe24eb27bbda8ec366a1d2bdad301fa98c946
Md Sazid <141584613+mdsazid239@users.noreply.github.com>
2025-07-05 12:30:11 +0530

    Add files via upload

---
+++ cheakeven.c
@@ -0,0 +1,28 @@
+
+// cheak a even number 
+// #include<stdio.h>
+// #include<math.h>
+// int main ()
+// {
+//  for(int i=1;i<=100;i++)
+//  if (i %2 !=0)
+//  {
+//     printf("%d\n",i);
+//  }
+//  return 0;
+// }
+
+
+// cheak number odd or even nummber
+
+// #include<stdio.h>
+// #include<math.h>
+// int main ()
+// {
+//  for(int i= 1; i<=100;i++)
+//  if (i%2==0){
+//     printf("%d\n",i);
+//  }
+//  return 0;
+// }
+
