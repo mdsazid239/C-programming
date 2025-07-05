d42d01899b3059dceaf9c451d0f30cc40aa26453
Md Sazid <141584613+mdsazid239@users.noreply.github.com>
2025-07-05 12:28:46 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,17 @@
+
+//  cheak a even or odd number 
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    for(int a= 1; a <=100; a=a+2)
+    {
+        if (a % 2==0)
+        {
+            printf("%d is an even number\n", a);
+        }
+    }
+return 0;
+}
+    
+
