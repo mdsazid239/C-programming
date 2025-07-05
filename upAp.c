8d65a4509c0196708036d81ced0e23b5ff10251a
Md Sazid <141584613+mdsazid239@users.noreply.github.com>
2025-07-05 12:26:45 +0530

    Add files via upload

---
+++ upAp.c
@@ -0,0 +1,15 @@
+// Display 4,7,10,13,16 up to n terms print values
+
+// #include<stdio.h>
+// #include<math.h>
+// int main ()
+// {
+//     int x;
+//     printf("enter a number");
+//     scanf("%d",&x);
+//     for(int i = 1 ; i<= 3*x+1 ; i = i + 3)
+//     {
+//         printf("%d \n", i);
+//     }
+//     return 0;
+// }
